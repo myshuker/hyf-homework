379c01eced4078b4df578194bdac433b48427b13
Mohammad alshoker <myshuker@gmail.com>
2020-03-03 09:06:03 +0100

    Update index.js

--- nodejs/week1/Meal_sharing_website/src/backend/index.js
+++ nodejs/week1/Meal_sharing_website/src/backend/index.js
@@ -4,20 +4,20 @@ const app = express();
 const getAllMeals = require("./routes/meals");
 app.get("/meals", getAllMeals);
 
-// const getCheapMeals = require("./routes/cheap-meals");
-// app.get("/cheap-meals", getCheapMeals);
+ const getCheapMeals = require("./routes/cheap-meals");
+ app.get("/cheap-meals", getCheapMeals);
 
-// const getLargeMeals = require("./routes/large-meals");
-// app.get("/large-meals", getLargeMeals);
+ const getLargeMeals = require("./routes/large-meals");
+ app.get("/large-meals", getLargeMeals);
  
-// const getRandomMeal = require("./routes/meal");
-// app.get("/meal", getRandomMeal);
+ const getRandomMeal = require("./routes/meal");
+ app.get("/meal", getRandomMeal);
 
-//  const getAllReservations = require("./routes/reservations");
-// app.get("/reservations", getAllReservations);
+ const getAllReservations = require("./routes/reservations");
+ app.get("/reservations", getAllReservations);
 
-// const getRandomReservation = require("./routes/reservation");
-// app.get("/reservation", getRandomReservation);
+const getRandomReservation = require("./routes/reservation");
+app.get("/reservation", getRandomReservation);
 
 app.listen(3000, listening);
 
