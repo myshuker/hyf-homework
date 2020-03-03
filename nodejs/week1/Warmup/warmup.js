dc1fa93bff1d37365b2f9d1cf3597e121038cd00
Mohammad alshoker <myshuker@gmail.com>
2020-03-04 00:24:40 +0100

    modify the code

--- nodejs/week1/Warmup/warmup.js
+++ nodejs/week1/Warmup/warmup.js
@@ -2,28 +2,18 @@
 // Circle calculator
 
 class Circle {
-    constructor(radius) {
-        this.radius = radius;
-     }
-
-     
-     getDiameter (){
-        this.diameter = 2 * this.radius;
-        console.log(this.diameter);
-     }
-
-     getCircumference (){
-        this.circumference = 2 * Math.PI * this.radius;
-        const ccumFix = this.circumference.toFixed(2);
-        console.log(ccumFix);
-    }
-
-    getArea (){
-        this.area = Math.PI * this.radius * this.radius;
-        const areaFix = this.area.toFixed(2);
-        console.log(areaFix);
-    }
-     
+constructor(radius) {
+this.radius = radius;
+}
+getDiameter (){
+return 2 * this.radius;
+}
+getCircumference (){
+return( 2 * Math.PI * this.radius).toFixed(2);
+}
+getArea (){
+return( Math.PI * this.radius * this.radius).toFixed(2);
+}
 }
 const circle = new Circle(10);
 circle.getDiameter();
