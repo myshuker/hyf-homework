2e64985ba7edb799bbfe1590a9ba7a86795e4129
Mohammad alshoker <myshuker@gmail.com>
2020-03-04 12:50:45 +0100

    modify warmup code

--- nodejs/week1/Warmup/warmup.js
+++ nodejs/week1/Warmup/warmup.js
@@ -2,20 +2,24 @@
 // Circle calculator
 
 class Circle {
-constructor(radius) {
-this.radius = radius;
-}
-getDiameter (){
-return 2 * this.radius;
-}
-getCircumference (){
-return( 2 * Math.PI * this.radius).toFixed(2);
-}
-getArea (){
-return( Math.PI * this.radius * this.radius).toFixed(2);
-}
-}
+    constructor(radius) {
+    this.radius = radius;
+    }
+    getDiameter (){
+    return 2 * this.radius;
+    }
+    getCircumference (){
+    return( 2 * Math.PI * this.radius);
+    }
+    getArea (){
+    return( Math.PI * this.radius * this.radius);
+    }
+    }
 const circle = new Circle(10);
-circle.getDiameter();
-circle.getCircumference();
-circle.getArea();
+console.log("getDiameter :", circle.getDiameter());
+console.log("getCircumference :", circle.getCircumference());
+console.log("getArea :", circle.getArea());
+
+
+
+
