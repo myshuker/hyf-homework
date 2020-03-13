18a1ad0ad1ee10846bf37edad7e04c9cbaa44856
Mohammad alshoker <myshuker@gmail.com>
2020-03-13 22:24:40 +0100

    update reservations code

--- nodejs/week3/db_meals_app/src/backend/api/reservations.js
+++ nodejs/week3/db_meals_app/src/backend/api/reservations.js
@@ -3,29 +3,28 @@ const app = express();
 const router = express.Router();
 const pool = require("./../database");
 
+// Get all reservations
 router.route("/")
-  .get((request, response) => {
-    const query = pool.query("SELECT * FROM reservations", (error, results, fields) => {
-        if (error) {
-          response.status(404).json({
-            msg: "Check Query",
-            Query: query.sql
-          });
-        }
-        console.log("Query :", query.sql);
-        response.send(results);
+.get((request, response) => {
+  const query = pool.query("SELECT * FROM reservations", (error, results, fields) => {
+      if (error) {
+        response.status(404).json({
+          msg: "Check Query",
+          Query: query.sql
+        });
       }
-    );
-  })
-
-  .post((request, response) => {
-   
+      console.log("Query :", query.sql);
+      response.send(results);
+    }
+  );
+})
+ // insert new reservations
+  .post((request, response) => {   
     const {number_of_guests, meal_id, location, created_date} = request.body;
     const reservation = {
         number_of_guests: number_of_guests,
         meal_id: meal_id,
-        created_date: created_date
-      
+        created_date: created_date      
     };
     const query = pool.query("INSERT INTO reservations SET ?", reservation,(error, results, fields) => {
         if (error) {
@@ -34,15 +33,16 @@ router.route("/")
             Query: query.sql
           });
         }
+        console.log("Query :", query.sql);
         response.status(201).json(results);
       }
     );
   });
-
+   // Get one reservation with id
 router.route("/:id")
       .get((request, response) => {
-        const id = request.params.id;
-        const query = pool.query("SELECT * FROM reservations where id ?", id,(error, results, fields) => {
+        const {id} = request.params;
+        const query = pool.query("SELECT * FROM reservations where id = ?", id,(error, results, fields) => {
             if (error) {
               response.status(404).json({
                 msg: "Check Query",
@@ -50,13 +50,15 @@ router.route("/:id")
               });
             }
             console.log("Query :", query.sql);
-            response.status(201).send(`reservation with id : ${id} Added` );
+            response.json(results);
           }
         );
       })
+      // update reservations
       .put((request, response) => {
         const id = request.params.id;
-        const query = pool.query("UPDATE reservations SET title = 'new_name_reservation'  WHERE id = ? ", id,(error, results, fields) => {
+       const {number_of_guests, meal_id, location, created_date} = request.body;
+        const query = pool.query("UPDATE reservations SET ?  WHERE id = ? ", [{number_of_guests, meal_id, location, created_date},id],(error, results, fields) => {
             if (error) {
               response.status(404).json({
                 msg: "Check Query",
@@ -68,9 +70,10 @@ router.route("/:id")
           }
         );
       })
+  // Delete one reservation with id
       .delete((request, response) => {
         const id = request.params.id;
-        const query = pool.query("DELETE FROM reservations where id ?", id,(error, results, fields) => {
+        const query = pool.query("DELETE FROM reservations WHERE id =?", id,(error, results, fields) => {
             if (error) {
               response.status(404).json({
                 msg: "Check Query",
