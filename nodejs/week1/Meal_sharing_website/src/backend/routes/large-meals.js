3bfd213f7cfe127ef9cea5b00d3441eaf663e8f9
Mohammad alshoker <myshuker@gmail.com>
2020-03-27 14:57:06 +0100

    change large-meals code

--- nodejs/week1/Meal_sharing_website/src/backend/routes/large-meals.js
+++ nodejs/week1/Meal_sharing_website/src/backend/routes/large-meals.js
@@ -1,23 +1,50 @@
 const jsonMeals = require("../data/meals.json");
 const jsonReview = require("../data/reviews.json");
 
-const getLargeMeals = function(req, res) {
-  const largMealReview = jsonMeals.map(data => {
-    data.review ==
-      jsonReview.filter(review => {
-        return data.id == review.mealId;
-      });
-
-    return data;
+const getLargeMeals = (req, res) => {
+  
+ const lotsOfPeople = jsonMeals.filter(meal => meal.maxNumberOfGuests > 5);
+  const mealReviews = lotsOfPeople.map(meal => {
+    return {
+      ...meal,
+      review: jsonReview.filter(review => review.mealId === meal.id)
+    };
   });
-
-  const largMealGuest = jsonMeals.filter(meal => {
-    if (meal.maxNumberOfGuests > 5) {
-      return meal;
-    }
-  });
-
-  res.json(largMealGuest);
+  res.json(mealReviews);
 };
 
+
 module.exports = getLargeMeals;
+ 
+
+
+
+
+
+
+// const jsonMeals = require("../data/meals.json");
+// const jsonReview = require("../data/reviews.json");
+
+// const getLargeMeals = function(req, res) {
+//   const largMealReview = jsonMeals.map(data => {
+//     data.review ==
+//       jsonReview.filter(review => {
+//         return data.id == review.mealId;
+//       });
+
+//     return data;
+//   });
+
+//   const largMealGuest = jsonMeals.filter(meal => {
+//     if (meal.maxNumberOfGuests > 5) {
+//       return meal;
+//     }
+//   });
+
+//   res.json(largMealGuest);
+// };
+
+// module.exports = getLargeMeals;
+
+
+
