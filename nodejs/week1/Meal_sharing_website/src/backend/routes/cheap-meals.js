92ffb891d28f4e23528de1cdbe8acf36c7542163
Mohammad alshoker <myshuker@gmail.com>
2020-03-27 01:39:00 +0100

    add new way to solve it

--- nodejs/week1/Meal_sharing_website/src/backend/routes/cheap-meals.js
+++ nodejs/week1/Meal_sharing_website/src/backend/routes/cheap-meals.js
@@ -17,3 +17,24 @@ module.exports = getCheapMeals;
 
 
 
+
+
+/* 
+// new way to solve it 
+const jsonMeals = require("../data/meals.json");
+const jsonReview = require("../data/reviews.json");
+
+const getCheapMeal = (req, res) => {
+  const cheap_meal = jsonMeals.filter(meal => meal.price < 70);
+
+  const mealReviews = cheap_meal.map(meal => {
+    return {
+      ...meal,
+      review: jsonReview.filter(review => review.mealId === meal.id)
+    };
+  });
+  res.json(mealReviews);
+};
+
+module.exports = getCheapMeal;
+ */
