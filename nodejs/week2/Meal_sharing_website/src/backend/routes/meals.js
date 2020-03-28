f75dcf50934931c4e9e47d395486081493ff1f86
Mohammad alshoker <myshuker@gmail.com>
2020-03-28 03:13:19 +0100

    repair meals code

--- nodejs/week2/Meal_sharing_website/src/backend/routes/meals.js
+++ nodejs/week2/Meal_sharing_website/src/backend/routes/meals.js
@@ -1,75 +1,41 @@
+const jsonMeals = require("../data/meals.json");
 const express = require("express");
 const router = express.Router();
-const jsonMeals = require("../data/meals.json");
 
-function getMeals(req, res) {
-  const idString = req.params.id;
-  const id = parseInt(idString);
-  const max_price = parseInt(req.query.maxPrice);
+const getMeal = (req, res) => {
+  const id = req.params.id;
+  const maxPrice = req.query.maxPrice;
   const title = req.query.title;
   const createdAfter = req.query.createdAfter;
-  const limit = parseInt(req.query.limit);
-
+  const limit = req.query.limit;
+  // res.send(id)
   if (id) {
-    if (id === 0 || id > jsonMeals.length)
-      res.status(404).send(`The item with id: ${id} was not found`);
-    const data = jsonMeals.filter(item => {
-      if (item.id === id) {
-        return item;
-      }
-    });
-
-    // console.log(data);
-    res.json(data);
-  } else if (idString) {
-    // console.log(typeof idString);
-    res.send(`Id it should be a number`);
-  } else if (max_price) {
-    const dataPrice = jsonMeals.filter(item => {
-      return item.price < max_price;
-    });
-    res.json(dataPrice);
+    const mealById = jsonMeals.filter(meal => meal.id == id);
+    res.json(mealById);
+  } else if (maxPrice) {
+    const mealsByMaxPrice = jsonMeals.filter(meal => meal.price < maxPrice);
+    res.json(mealsByMaxPrice);
   } else if (title) {
-    const meal_title = jsonMeals.filter(item => {
-      return item.title.toLowerCase() === title.toLowerCase();
+    const splitTitle = title.split(" ");
+    console.log(splitTitle);
+    const mealsByTitle = jsonMeals.filter(meal => {
+      if (meal.title.include(splitTitle)) return meal;
     });
-    res.json(meal_title);
+    res.json(mealsByTitle);
   } else if (createdAfter) {
-    const meal_created_after = jsonMeals.filter(item => {
-      return item.createdAt > createdAfter;
-    });
-    res.json(meal_created_after);
+    const mealsByCreatedAfter = jsonMeals.filter(
+      meal => meal.createdAt > createdAfter
+    );
+    res.json(mealsByCreatedAfter);
   } else if (limit) {
-    const meal_limit = jsonMeals.slice(limit);
-    res.json(meal_title);
-  } else if (!id) {
+    const mealsByLimit = jsonMeals.slice(0, limit);
+    res.json(mealsByLimit);
+  } else {
     res.json(jsonMeals);
   }
-}
+};
 
-router.get("/", getMeals);
-router.get("/:id", getMeals);
+router.get("/", getMeal);
+router.get("/:id", getMeal);
 
 module.exports = router;
-
-//function getMeals(req, res) {
-
-// const id = parseInt(req.params.id);
-// const data = jsonMeals.find(item => item.id === id);
-
-// // if(id || data){
-//   if (id === 0 || id > jsonMeals.length)
-//   res.status(404).send(`The item with id: ${id} was not found`);
-//   else if (!data) res.json(jsonMeals);
-
-//   res.json(data);
-
-//   const dataPrice = jsonMeals.filter(item => {
-//     const max_price = parseInt(req.query.maxPrice);
-//     return item.price < max_price
-
-//     });
-// res.json(dataPrice)
-//   }
-
-// }
