9526e7f94567c72e827ddbd4254cb988e4ce72b0
Mohammad alshoker <myshuker@gmail.com>
2020-03-28 01:50:55 +0100

    add some code to reservatios

--- nodejs/week2/Meal_sharing_website/src/backend/routes/reservations.js
+++ nodejs/week2/Meal_sharing_website/src/backend/routes/reservations.js
@@ -28,3 +28,25 @@ router.get("/", allReservations);
 router.get("/:id", allReservations);
 
 module.exports = router;
+
+
+//or
+const jsonReservations = require("../data/reservations.json");
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.json(jsonReservations);
+// });
+
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   // res.send(id)
+//   const reservationBYId = jsonReservations.filter(
+//     reservation => reservation.id == id
+//   );
+//   res.json(reservationBYId);
+// });
+
+// module.exports = router;
+
