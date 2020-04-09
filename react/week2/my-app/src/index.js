19572a8d80ad153494518604e9e0bef693f3aba1
Mohammad alshoker <myshuker@gmail.com>
2020-04-09 20:25:46 +0200

    Index.js updated

--- react/week2/my-app/src/index.js
+++ react/week2/my-app/src/index.js
@@ -1,33 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import GetTodoList from "./App";
+import App from "./App";
 import Counter from "./Counter";
 
-const root = document.getElementById("root");
-
-function Header() {
-  return (
-    <div>
-      <h1>Todo List App</h1>
-    </div>
-  );
-}
-
-function Footer() {
-  return <h3>Enjoy with a fresh day</h3>;
-}
-
-class App extends React.Component {
+class Main extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <h1>Todo List App</h1>
         <Counter initialCounter={0} />
-        <GetTodoList />
-        <Footer />
+        <App />
+        <h3>Enjoy with a fresh day</h3>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, root);
+const root = document.getElementById("root");
+ReactDOM.render(<Main />, root);
+
