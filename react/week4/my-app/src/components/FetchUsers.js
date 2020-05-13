4d6fc730937a160f8e0ed31a676f2123b78ae800
Mohammad alshoker <myshuker@gmail.com>
2020-05-13 17:08:49 +0200

    comments deleted

--- react/week4/my-app/src/components/FetchUsers.js
+++ react/week4/my-app/src/components/FetchUsers.js
@@ -1,18 +1,9 @@
-import React from 'react';
+import React from "react";
 import context from "../context";
 
-
-
 function FetchUsers(inputValue) {
-
- // const { inputValue } = React.useContext(context);
-
-  const URL = `https://api.github.com/search/users?q=${inputValue}`
- return fetch(URL)
- .then(res =>res.json())
-
-  }
-  
-
+  const URL = `https://api.github.com/search/users?q=${inputValue}`;
+  return fetch(URL).then((res) => res.json());
+}
 
 export default FetchUsers;
